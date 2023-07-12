9af285ebcebd9e7b1dfc080e8d6111af4361db4d
Blair Stacey <Bobbias@gmail.com>
2023-07-12 13:35:08 -0400

    Make Token properties public.

--- CsLox/Token.cs
+++ CsLox/Token.cs
@@ -30,10 +30,10 @@ namespace CsLox
             EOF
         }
 
-        TokenType Type { get; }
-        string Lexeme { get; }
-        object? Literal { get; }
-        int Line { get; }
+        public TokenType Type { get; }
+        public string Lexeme { get; }
+        public object? Literal { get; }
+        public int Line { get; }
 
         public Token(TokenType type, string lexeme, object? literal, int line)
         {
