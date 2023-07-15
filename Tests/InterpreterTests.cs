75a7967a345dfdbc1793cf960d0965bddbcc9c0c
Blair Stacey <Bobbias@gmail.com>
2023-07-15 09:37:44 -0400

    Tests: Add basic variables test script/test.

--- Tests/InterpreterTests.cs
+++ Tests/InterpreterTests.cs
@@ -3,12 +3,14 @@ namespace Tests
     public class InterpreterTests
     {
         private string printsSource;
+        private string variablesSource;
 
         [SetUp]
         public void Setup()
         {
 
             printsSource = FixtureFileLoader.LoadFileToString("Scripts", "prints.lox");
+            variablesSource = FixtureFileLoader.LoadFileToString("Scripts", "variables.lox");
         }
 
         [Test]
@@ -21,6 +23,21 @@ namespace Tests
                 CLI.Run(printsSource);
 
                 var expected = "one\r\nTrue\r\n3\r\n";
+                Assert.That(sw.ToString(), Is.EqualTo(expected));
+            }
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+        }
+
+        [Test]
+        public void PrintsVariables()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                CLI.Run(variablesSource);
+
+                var expected = "3\r\n";
                 Assert.That(sw.ToString(), Is.EqualTo(expected));
             }
             Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
