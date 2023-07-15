d54c1db1c4537e0958cda0fca7a3ee6466772e19
Blair Stacey <Bobbias@gmail.com>
2023-07-15 09:34:37 -0400

    Tool: Add variables and assignment.

--- GenerateAst/AstBuilder.cs
+++ GenerateAst/AstBuilder.cs
@@ -33,7 +33,8 @@ namespace GenerateAst
                      * statement      → exprStmt | printStmt ;
                      * exprStmt       → expression ";" ;
                      * printStmt      → "print" expression ";" ;
-                     * expression     → equality ;
+                     * expression     → assignment ;
+                     * assignment     → IDENTIFIER "=" assignment | equality ;
                      * equality       → comparison ( ( "!=" | "==" ) comparison )* ;
                      * comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
                      * term           → factor ( ( "-" | "+" ) factor )* ;
--- GenerateAst/CLI.cs
+++ GenerateAst/CLI.cs
@@ -13,17 +13,20 @@ namespace GenerateAst
             var file = new FileInfo("Expr.cs");
 
             AstBuilder.DefineAst(file, new List<string> {
+                "Assign   : Token name, Expr value",
                 "Binary   : Expr left, Token oper, Expr right",
                 "Grouping : Expr expression",
                 "Literal  : object? value",
-                "Unary    : Token oper, Expr right"
+                "Unary    : Token oper, Expr right",
+                "Variable : Token name"
             });
 
             file = new FileInfo("Stmt.cs");
 
             AstBuilder.DefineAst(file, new List<string> {
-              "Expression : Expr expr",
-              "Print      : Expr expr"
+                "Expression : Expr expr",
+                "Print      : Expr expr",
+                "Var        : Token name, Expr initializer"
             });
         }
     }
