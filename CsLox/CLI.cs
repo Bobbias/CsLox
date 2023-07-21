15524faca25d7f3b5455b6ca20a6e34457ba38c2
Blair Stacey <Bobbias@gmail.com>
2023-07-20 22:54:43 -0400

    Sort ConsoleMode and StandardIOHandles.

--- CsLox/CLI.cs
+++ CsLox/CLI.cs
@@ -10,7 +10,10 @@ using static CsLox.Token;
 
 namespace CsLox
 {
-    public static class CLI
+    /// <summary>
+    /// Contains commands for the command line interface.
+    /// </summary>
+    public static partial class CLI
     {
         /// <summary>
         /// Indicates whether the CLI environment we're running in supports ANSI escape codes.
@@ -274,22 +277,22 @@ namespace CsLox
             [Flags]
             private enum ConsoleMode : uint
             {
+                NONE = 0x0000,
                 ENABLE_PROCESSED_INPUT = 0x0001,
+                ENABLE_PROCESSED_OUTPUT = 0x0001,
                 ENABLE_LINE_INPUT = 0x0002,
+                ENABLE_WRAP_AT_EOL_OUTPUT = 0x0002,
                 ENABLE_ECHO_INPUT = 0x0004,
+                ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004,
                 ENABLE_WINDOW_INPUT = 0x0008,
+                DISABLE_NEWLINE_AUTO_RETURN = 0x0008,
                 ENABLE_MOUSE_INPUT = 0x0010,
+                ENABLE_LVB_GRID_WORLDWIDE = 0x0010,
                 ENABLE_INSERT_MODE = 0x0020,
                 ENABLE_QUICK_EDIT_MODE = 0x0040,
                 ENABLE_EXTENDED_FLAGS = 0x0080,
                 ENABLE_AUTO_POSITION = 0x0100,
-                ENABLE_VIRTUAL_TERMINAL_INPUT = 0x0200,
-
-                ENABLE_PROCESSED_OUTPUT = 0x0001,
-                ENABLE_WRAP_AT_EOL_OUTPUT = 0x0002,
-                ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004,
-                DISABLE_NEWLINE_AUTO_RETURN = 0x0008,
-                ENABLE_LVB_GRID_WORLDWIDE = 0x0010
+                ENABLE_VIRTUAL_TERMINAL_INPUT = 0x0200
             }
 
             /// <summary>
@@ -297,9 +300,9 @@ namespace CsLox
             /// </summary>
             private enum StandardIOHandles : int
             {
-                STD_INPUT_HANDLE = -10,
+                STD_ERROR_HANDLE = -12,
                 STD_OUTPUT_HANDLE = -11,
-                STD_ERROR_HANDLE = -12
+                STD_INPUT_HANDLE = -10
             }
 
             /// <summary>
